a30cb86e8a6fa5257ff4b428b8864e34d3b149ac
jawlinski <pauljawlinski@yahoo.de>
2023-05-22 18:12:22 +0200

    Aufruf zweite und dritte Activty

---
+++ app/src/androidTest/java/com/example/activitydemo2/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.example.activitydemo2;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String RETVAL_KEY="RETURN STRING";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        final TextView lblAusgabe = (TextView) findViewById(R.id.lblAusgabe);
+        lblAusgabe.setText(getIntent().getExtras().getString(MainActivity.KEY));
+        Button btnBack = (Button) findViewById(R.id.btnZurueck);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultData = new Intent();
+                resultData.putExtra(RETVAL_KEY,"->: " + lblAusgabe.getText());
+                setResult(Activity.RESULT_OK,resultData);
+                finish();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/activitydemo2/SecondActivity.java
@@ -0,0 +1,33 @@
+
+package com.example.activitydemo2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String RETVAL_KEY="RETURN STRING";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        final TextView lblAusgabe = (TextView) findViewById(R.id.lblAusgabe);
+        lblAusgabe.setText("Zweite Activity " + getIntent().getExtras().getString(MainActivity.KEY));
+        Button btnBack = (Button) findViewById(R.id.btnZurueck);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultData = new Intent();
+                resultData.putExtra(RETVAL_KEY,"->: " + lblAusgabe.getText());
+                setResult(Activity.RESULT_OK,resultData);
+                finish();
+
+            }
+        });
+    }
+}
